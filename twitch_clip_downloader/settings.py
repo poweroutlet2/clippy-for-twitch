82eb99544faeabd3ba5c26ec1ac0db1ecace5b67
patel2468 <32393047+patel2468@users.noreply.github.com+harshp2015m@gmail.com>
2021-07-05 00:59:41 -0400

    Added django-heroku

--- twitch_clip_downloader/settings.py
+++ twitch_clip_downloader/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,7 @@ SECRET_KEY = os.environ.get('CLIPPY_SECRET_KEY')
 DEBUG = True #os.environ.get('DEBUG') 
 
 ALLOWED_HOSTS = [
-    'https://clippyfortwitch.herokuapp.com/',
+    'clippyfortwitch.herokuapp.com/',
 ]
 # Application definition
 
@@ -131,3 +132,5 @@ MEDIA_ROOT = '/media/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
