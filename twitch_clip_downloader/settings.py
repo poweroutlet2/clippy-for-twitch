4e2cfd35c1cb7df879c35e20a8e52892401c2f3a
patel2468 <32393047+patel2468@users.noreply.github.com+harshp2015m@gmail.com>
2021-07-05 00:39:30 -0400

    Static Root add

--- twitch_clip_downloader/settings.py
+++ twitch_clip_downloader/settings.py
@@ -119,10 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
